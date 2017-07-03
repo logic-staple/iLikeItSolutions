52d2ac3d27b4058652480f2a05f0b45c1709726f
piyush_sl <piyush@staplelogic.in>
2017-07-03 11:42:39 +0530

    updated code

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace iLikeItSolutions\GoogleAnalyticsApiBundle\Controller;
 
+use Google_Service_Analytics;
 use Google_Service_AnalyticsReporting_DateRange;
 use Google_Service_AnalyticsReporting_GetReportsRequest;
 use Google_Service_AnalyticsReporting_Metric;
@@ -10,33 +11,86 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 
+//https://developers.google.com/analytics/devguides/config/mgmt/v3/quickstart/web-php
 class DefaultController extends Controller
 {
     /**
      * @Route("/analytic", name="homepage")
      * @Template()
+     * purpose: Fetch all dashboard data using GA API
      */
-    //public function indexAction($viewId)
-    public function indexAction()
+   	
+    public function indexAction(Request $requestObj)
     {
-
     	$session = $this->get('session');
-    	if($session->has('access_token')){
+    	/* Get access token detail from file and refresh if token expired */
+        $tokenFileLocation = __DIR__.'/../Data/token.json';
 
-	    	$viewId = "106341934";
+        $clientFileLocation = __DIR__.'/../Data/client_secrets.json';
+        $isClientFileExist = 0;
 
+        if(file_exists($clientFileLocation)){
+        	$isClientFileExist = 1;
+        }
+
+        $viewId = $this->getParameter('ga_view_id');
+
+        /* Check first Client Json file exist */
+        if($isClientFileExist == 0 || empty($viewId)){
+        	return $this->render('AnalyticBundle:Default:index.html.twig', array(
+        			'isClientFileExist' => $isClientFileExist,
+        			'viewId' => $viewId
+        		));
+        }
+
+        /* Check Token JSON file exist if not then redirect to auth first time from GA */
+        if(!file_exists($tokenFileLocation)){
+        	return $this->redirectToRoute('google_analytics_api_callback', array(), 301);
+        }
+        $tokenJSONData = file_get_contents($tokenFileLocation); 
+        $tokenData = json_decode($tokenJSONData, true);
+        $acessToken = isset($tokenData['access_token'])?$tokenData['access_token']:"";
+        $refreshToken = isset($tokenData['refresh_token'])?$tokenData['refresh_token']:"";
+        
+    	if(!empty($acessToken)){
 	        $analyticsService = $this->get('google_analytics_api.api');
-
-	        $client = $analyticsService->getClient();
-	        $client->setAccessToken($session->get('access_token'));
-
+	    try{
+        	$client = $analyticsService->getClient();
+        	$client->setAccessToken(json_encode($tokenData));
+        }catch(Exception $e){
+        	echo "Exception: ".$e->getMessage();
+        }
+	        $isExpired = $client->isAccessTokenExpired();
+	        if($isExpired){
+				$new_access_token = $client->refreshToken($refreshToken);
+				if(isset($new_access_token['access_token']) && !empty($new_access_token['access_token'])){
+					$new_access_token = json_encode($new_access_token);
+					file_put_contents($tokenFileLocation,$new_access_token);
+					$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/analytic';
+					$redirect_uri =   filter_var($redirect_uri, FILTER_SANITIZE_URL);
+					return $this->redirect($redirect_uri);
+					
+				}
+			}
+			$beginDate = date('Y-m-d', strtotime('-29 days'));
+			$endDate = date('Y-m-d', time());
+			if($requestObj->query->get('beginDate')){
+				$beginDate = date("Y-m-d", strtotime($requestObj->query->get('beginDate')));
+			}
+			if($requestObj->query->get('endDate')){
+				$endDate = date("Y-m-d", strtotime($requestObj->query->get('endDate')));
+			}
+			
 	        $analytics = $analyticsService->getAnalytics();
 
 	        // Create the DateRange object
 	        $dateRange = new Google_Service_AnalyticsReporting_DateRange();
-	        $dateRange->setStartDate("30daysAgo");
-	        $dateRange->setEndDate("today");
+	        $dateRange->setStartDate($beginDate);
+	        $dateRange->setEndDate($endDate);
 
 	        // Create the Metrics object
 	        $sessions = new Google_Service_AnalyticsReporting_Metric();
@@ -55,19 +109,48 @@ class DefaultController extends Controller
 
 	        // above code included into this helper method :
 
-	        $sessions = $analyticsService->getSessionsDateRange($viewId,'30daysAgo','today');
-	        $bounceRate = $analyticsService->getBounceRateDateRange($viewId,'30daysAgo','today');
-	        $avgTimeOnPage = $analyticsService->getAvgTimeOnPageDateRange($viewId,'30daysAgo','today');
-	        $pageViewsPerSession = $analyticsService->getPageviewsPerSessionDateRange($viewId,'30daysAgo','today');
-	        $percentNewVisits = $analyticsService->getPercentNewVisitsDateRange($viewId,'30daysAgo','today');
-	        $pageViews = $analyticsService->getPageViewsDateRange($viewId,'30daysAgo','today');
-	        $avgPageLoadTime = $analyticsService->getAvgPageLoadTimeDateRange($viewId,'30daysAgo','today');
-	        //echo "<pre>"; print_r($bounceRate); die;
-	        //return $this->render('AnalyticBundle:Default:index.html.twig');
+	        $sessions = $analyticsService->getSessionsDateRange($viewId,$beginDate,$endDate);
+	        $bounceRate = $analyticsService->getBounceRateDateRange($viewId,$beginDate,$endDate);
+	        $avgTimeOnPage = $analyticsService->getAvgTimeOnPageDateRange($viewId,$beginDate,$endDate);
+	        $pageViewsPerSession = $analyticsService->getPageviewsPerSessionDateRange($viewId,$beginDate,$endDate);
+	        $percentNewVisits = $analyticsService->getPercentNewVisitsDateRange($viewId,$beginDate,$endDate);
+	        $pageViews = $analyticsService->getPageViewsDateRange($viewId,$beginDate,$endDate);
+	        $avgPageLoadTime = $analyticsService->getAvgPageLoadTimeDateRange($viewId,$beginDate,$endDate);
+	        
+	        $limit = 5; // set limit to 5 for table records
+	        $trafficSourcesData = $trafficLocationData = $pagesVisitData = $geoLocationDataResult = $pagesSessionsData = array();
+	        /* Get Traffic source data with domain name */
+	       	$trafficSourcesData = $analyticsService->getTraffic($viewId,$beginDate,$endDate, 'ga:sessions', 'ga:source', $limit);
+	        /* Get Traffic source data with location name */
+	        $trafficLocationData = $analyticsService->getTraffic($viewId,$beginDate,$endDate, 'ga:sessions', 'ga:country', $limit);
+	        /* Get Traffic source data with pages name */
+	        $pagesVisitData = $analyticsService->getPagesVisits($viewId,$beginDate,$endDate);
+	        $limit = "All";
+	        /* Get all traffic sessions on different location for geo map */
+	        $geoLocationDataResult = $analyticsService->getTraffic($viewId,$beginDate,$endDate, 'ga:sessions', 'ga:country', $limit);
+	        $geoLocationData = array();
+	        foreach($geoLocationDataResult as $d){
+	       			$arr = array($d['source'], $d['value']);
+	        		array_push($geoLocationData, $arr);
+	        }
+
+	        $currentWeek = date('W', time());
+	    	$year = date('Y', time());
+	    	$lastWeek = ($currentWeek - 1);
+	    	$lastWeekStart = date("Y-m-d", strtotime("{$year}-W{$lastWeek}-0"));
+	    	$lastWeekEnd = date("Y-m-d", strtotime("{$year}-W{$lastWeek}-6"));
+
+	    	/* Get last week all days sessions/pagevisits line graph data */
+	        $pagesSessionsData = $analyticsService->getPagesSessionWeekly($viewId,$lastWeekStart,$lastWeekEnd);
+	
 	        return $this->render('AnalyticBundle:Default:index.html.twig', array(
 	            'analytics'     =>  $analytics,
 	            'client'        =>  $client,
 	            'report'        =>  $report,
+	            'beginDate'		=>  date("Y-m-d", strtotime($beginDate)),
+	            'endDate'		=>  date("Y-m-d", strtotime($endDate)),
+	            'isClientFileExist' => $isClientFileExist,
+	            'viewId'		=> 	$viewId,
 	            'data'          =>  [
 	                'sessions'              =>  $sessions,
 	                'bounce_rate'           =>  $bounceRate,
@@ -75,7 +158,12 @@ class DefaultController extends Controller
 	                'page_view_per_session' =>  $pageViewsPerSession,
 	                'percent_new_visits'    =>  $percentNewVisits,
 	                'page_views'            =>  $pageViews,
-	                'avg_page_load_time'    =>  $avgPageLoadTime
+	                'avg_page_load_time'    =>  $avgPageLoadTime,
+	                'trafficSourcesData' 	=> 	$trafficSourcesData,
+	                'trafficLocationData'	=>  $trafficLocationData,
+	                'pagesVisitData'		=> 	$pagesVisitData,
+	                'geoLocationData'       =>  $geoLocationData,
+	                'pagesSessionsData'		=>  $pagesSessionsData
 	            ]
 	        ));
 		}
@@ -88,24 +176,59 @@ class DefaultController extends Controller
 
     /**
      * Matches /callback exactly
-     *
+     * Purpose: For authorized GA access from account first time and
+     *			write recieved tokens to token.json file
      * @Route("/callback", name="google_analytics_api_callback")
      */
     public function callbackAction()
     {
     	$analyticsService = $this->get('google_analytics_api.api');
     	$client = $analyticsService->getClient();
-
+    	try{
+        	$client = $analyticsService->getClient();
+        }catch(Exception $e){
+        	echo "Exception: ".$e->getMessage();
+        }
         // Handle authorization flow from the server.
-		if (! isset($_GET['code']))
+		if (!isset($_GET['code']))
 		{
+			/* check first access token is not expired */
+			$tokenFileLocation = __DIR__.'/../Data/token.json';
+			/* Check Token JSON file exist if not then redirect to auth first time from GA */
+        	if(file_exists($tokenFileLocation)){
+		        $tokenJSONData = file_get_contents($tokenFileLocation); 
+		        $tokenData = json_decode($tokenJSONData, true);
+		        $acessToken = isset($tokenData['access_token'])?$tokenData['access_token']:"";
+		        $refreshToken = isset($tokenData['refresh_token'])?$tokenData['refresh_token']:"";
+	        	if(!empty($acessToken)){
+	        		$isExpired = $client->isAccessTokenExpired();
+			        if($isExpired){
+						$new_access_token = $client->refreshToken($refreshToken);
+						if(isset($new_access_token['access_token']) && !empty($new_access_token['access_token'])){
+							$new_access_token = json_encode($new_access_token);
+							file_put_contents($tokenFileLocation,$new_access_token);
+							$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/analytic';
+							$redirect_uri =   filter_var($redirect_uri, FILTER_SANITIZE_URL);
+							return $this->redirect($redirect_uri);
+						}
+					}
+	        	}
+        	}
+
 			$auth_url = $client->createAuthUrl();
 			$auth_url = filter_var($auth_url, FILTER_SANITIZE_URL);
 			return $this->redirect($auth_url);
 		} 
 		else
 		{
-			$client->authenticate($_GET['code']);
+			$cred = $client->authenticate($_GET['code']);
+			/* write refresh token file */
+			$token_json = json_encode($cred);
+			$tokenFileLocation = __DIR__.'/../Data/token.json';
+	    	$file = fopen($tokenFileLocation,"w");
+	    	fwrite($file,$token_json);
+	    	fclose($file);
+
 			$session = $this->get('session');
 			if (isset($_GET['code']))
 			{
@@ -116,6 +239,13 @@ class DefaultController extends Controller
 			return $this->redirect($redirect_uri);
 			//return $this->redirectToRoute('analytic_homepage', array(), 301);
 		}
+    }
+
+
+    public function uploadAction(Request $request){
+    	return $this->render('GoogleAnalyticsApiBundle:Default:upload.html.twig', array(
+        ));
+
     }
 }
 
--- Service/GoogleAnalyticsService.php
+++ Service/GoogleAnalyticsService.php
@@ -5,8 +5,11 @@ namespace iLikeItSolutions\GoogleAnalyticsApiBundle\Service;
 use Google_Client;
 use Google_Service_AnalyticsReporting;
 use Google_Service_AnalyticsReporting_DateRange;
+use Google_Service_AnalyticsReporting_OrderBy;
 use Google_Service_AnalyticsReporting_GetReportsRequest;
+use Google_Service_AnalyticsReporting_ReportData;
 use Google_Service_AnalyticsReporting_Metric;
+use Google_Service_AnalyticsReporting_Dimension;
 use Google_Service_AnalyticsReporting_ReportRequest;
 use Symfony\Component\Config\Definition\Exception\Exception;
 
@@ -36,9 +39,15 @@ class GoogleAnalyticsService {
         if (!file_exists($keyFileLocation)) {
             throw new Exception("can't find file key location defined by analytic.google_analytics_json_key parameter, ex : ../data/client_secrets.json");
         }
-
-        $this->client = new Google_Client();
+        try{
+            $this->client = new Google_Client();
+        }catch(Exception $e){
+            echo "Exception: ".$e->getMessage();
+        }
         //$this->client->setApplicationName("GoogleAnalytics");
+        $this->client->setAccessType("offline");        // offline access
+        $this->client->setIncludeGrantedScopes(true);   // incremental auth
+        //$this->client->addScope(Google_Service_Analytics::ANALYTICS_READONLY);
         $this->client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
         $this->client->setAuthConfig($keyFileLocation);
 
@@ -173,5 +182,167 @@ class GoogleAnalyticsService {
     public function getAvgPageLoadTimeDateRange($viewId,$dateStart,$dateEnd) {
         return $this->getDataDateRange($viewId,$dateStart,$dateEnd,'avgPageLoadTime');
     }
+
+    /* Get Trafiic data by source and country */
+    public function getTraffic($viewId,$dateStart,$dateEnd, $metr = "ga:sessions", $dim = "ga:source", $limit){
+        //https://github.com/google/google-api-php-client-services/blob/master/src/Google/Service/AnalyticsReporting/Dimension.php
+        // Create the DateRange object
+        //default take 23 May to 22 June ------ current date is 23 June
+        $dateRange = new Google_Service_AnalyticsReporting_DateRange();
+        $dateRange->setStartDate($dateStart);
+        $dateRange->setEndDate($dateEnd);
+        // Create the Metrics object
+        $metrics = new Google_Service_AnalyticsReporting_Metric();
+        $metrics->setExpression($metr);
+        $metrics->setAlias("sessions");
+
+        // Create the dimension object
+        $dimension = new Google_Service_AnalyticsReporting_Dimension();
+        $dimension->setName($dim);
+
+        $order_by = new Google_Service_AnalyticsReporting_OrderBy();
+        $order_by->setFieldName('sessions');
+        $order_by->setSortOrder('DESCENDING');
+
+        // Create the ReportRequest object
+        $request = new Google_Service_AnalyticsReporting_ReportRequest();
+        $request->setViewId($viewId);
+        $request->setDateRanges($dateRange);
+        $request->setMetrics([$metrics]);
+        $request->setDimensions([$dimension]);
+        $request->setOrderBys([$order_by]);
+        if($limit != "All"){
+            $request->setPageSize($limit); // set page size
+        }
+        $request->setIncludeEmptyRows(false);
+        $request->setSamplingLevel("SMALL");
+        $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
+        $body->setReportRequests([$request]);
+
+        $report = $this->analytics->reports->batchGet($body);
+
+        $result = $report->getReports()[0]->getData()->getRows();
+        $finalResult = array();
+        if(!empty($result)){
+            foreach ($result as $key => $value) {
+                $finalResult[] = array(
+                                        'source' => isset($value['dimensions'][0])?$value['dimensions'][0]:"N/A",
+                                        'value' => isset($value['metrics'][0]['values'][0])?$value['metrics'][0]['values'][0]:"N/A"
+                                        );
+            }
+        }
+        //echo "<pre>";print_r($report);exit;
+        return $finalResult;
+    }
+
+    /* Get Pages data visits and percentage */
+    public function getPagesVisits($viewId,$dateStart,$dateEnd){
+        //https://github.com/google/google-api-php-client-services/blob/master/src/Google/Service/AnalyticsReporting/Dimension.php
+        // Create the DateRange object
+        //default take 23 May to 22 June ------ current date is 23 June
+        $dateRange = new Google_Service_AnalyticsReporting_DateRange();
+        $dateRange->setStartDate($dateStart);
+        $dateRange->setEndDate($dateEnd);
+        
+        // Create the Metrics object
+        $metrics1 = new Google_Service_AnalyticsReporting_Metric();
+        $metrics1->setExpression("ga:pageviews");
+        $metrics1->setAlias("pageviews");
+
+        /*$metrics2 = new Google_Service_AnalyticsReporting_Metric();
+        $metrics2->setExpression("ga:entranceRate");
+        $metrics2->setAlias("percentage");*/
+
+        // Create the dimension object
+        $dimension = new Google_Service_AnalyticsReporting_Dimension();
+        $dimension->setName('ga:pagePath');
+
+        $order_by = new Google_Service_AnalyticsReporting_OrderBy();
+        $order_by->setFieldName('pageviews');
+        $order_by->setSortOrder('DESCENDING');
+
+        // Create the ReportRequest object
+        $request = new Google_Service_AnalyticsReporting_ReportRequest();
+        $request->setViewId($viewId);
+        $request->setDateRanges($dateRange);
+        $request->setMetrics([$metrics1]);
+        $request->setDimensions([$dimension]);
+        $request->setOrderBys([$order_by]);
+        $request->setPageSize(5); // set page size
+        $request->setIncludeEmptyRows(false);
+        $request->setSamplingLevel("SMALL");
+        $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
+        $body->setReportRequests([$request]);
+
+        $report = $this->analytics->reports->batchGet($body);
+
+        $result = $report->getReports()[0]->getData()->getRows();
+        //echo "<pre>";print_r($result);exit;
+        $finalResult = array();
+        if(!empty($result)){
+            foreach ($result as $key => $value) {
+                $finalResult[] = array(
+                                        'path' => isset($value['dimensions'][0])?$value['dimensions'][0]:"N/A",
+                                        'pageviews' => isset($value['metrics'][0]['values'][0])?$value['metrics'][0]['values'][0]:"N/A"
+                                        );
+            }
+        }
+        
+        return $finalResult;
+    }
+
+
+
+    /* Get Pages data visits and percentage */
+    public function getPagesSessionWeekly($viewId,$dateStart,$dateEnd){
+        // Metric 'ga:sessions,ga:pageviews',  Dimension 'ga:week'
+        $dateRange = new Google_Service_AnalyticsReporting_DateRange();
+        $dateRange->setStartDate($dateStart);
+        $dateRange->setEndDate($dateEnd);
+        
+        // Create the Metrics object
+        $metrics1 = new Google_Service_AnalyticsReporting_Metric();
+        $metrics1->setExpression("ga:sessions");
+        //$metrics1->setAlias("sessions");
+
+        $metrics2 = new Google_Service_AnalyticsReporting_Metric();
+        $metrics2->setExpression("ga:pageviews");
+        //$metrics2->setAlias("pageviews");
+
+        // Create the dimension object
+        $dimension = new Google_Service_AnalyticsReporting_Dimension();
+        $dimension->setName('ga:dayOfWeek');
+
+        $order_by = new Google_Service_AnalyticsReporting_OrderBy();
+        $order_by->setFieldName('ga:dayOfWeek');
+        $order_by->setSortOrder('ASCENDING');
+        
+        // Create the ReportRequest object
+        $request = new Google_Service_AnalyticsReporting_ReportRequest();
+        $request->setViewId($viewId);
+        $request->setDateRanges($dateRange);
+        $request->setMetrics([$metrics1, $metrics2]);
+        //$request->setMetrics([$metrics2]);
+        $request->setDimensions([$dimension]);
+        $request->setOrderBys([$order_by]);
+        //$request->setPageSize(5); // set page size
+        //$request->setIncludeEmptyRows(false);
+        //$request->setSamplingLevel("SMALL");
+        $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
+        $body->setReportRequests([$request]);
+
+        $report = $this->analytics->reports->batchGet($body);
+
+        $result = $report->getReports()[0]->getData()->getRows();
+        $finalResult = array();
+        if(!empty($result)){
+            foreach ($result as $key => $value) {
+                $finalResult['sessions'][] = $value['metrics'][0]['values'][0];
+                $finalResult['pageviews'][] = $value['metrics'][0]['values'][1];
+            }
+        }
+        return $finalResult;
+    }
+
 
 }
